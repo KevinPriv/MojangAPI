205d61d7383c4ece74e825baf455c1b79c48146c
KevinPriv <kb215@ga.newcastle.sch.uk>
2018-02-18 10:17:45 +0000

    Forgot available profile

---
+++ src/main/java/me/kbrewster/mojangapi/authentication/AvailableProfile.java
@@ -0,0 +1,19 @@
+package me.kbrewster.mojangapi.authentication;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+
+@Getter
+public class AvailableProfile {
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("legacy")
+    @Expose
+    private boolean legacy;
+
+}
